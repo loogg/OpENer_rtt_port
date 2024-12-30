a79c30d89a6438d65662457f3c7d7ff332ab7b75
mlw <2544047213@qq.com>
2024-12-30 22:20:09 +0800

    update devicedata.h

--- devicedata.h
+++ devicedata.h
@@ -7,11 +7,6 @@
 #ifndef DEVICE_DATA_H_
 #define DEVICE_DATA_H_
 
-#define OPENER_DEVICE_VENDOR_ID      (1)
-#define OPENER_DEVICE_TYPE           (12)
-#define OPENER_DEVICE_PRODUCT_CODE   (65001)
-#define OPENER_DEVICE_MAJOR_REVISION (2)
-#define OPENER_DEVICE_MINOR_REVISION (3)
-#define OPENER_DEVICE_NAME           ("OpENer PC")
+#include <rtthread.h>
 
 #endif /* DEVICE_DATA_H_ */
